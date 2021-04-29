adb6f092f8029e3971ab5df6d100d22fb2c6ee7e
Bob van Manen <bmanen@adobe.com>
2021-04-29 16:13:54 -0700

    Add to unit test coverage

--- test/generator.test.js
+++ test/generator.test.js
@@ -15,10 +15,14 @@
 'use strict';
 
 const assert = require('assert');
+const { AsyncGeneratorFunction } = require('../lib/generator/function');
 const { Flatten } = require('../lib/generator/flatten');
 const { Batch } = require('../lib/generator/batch');
 const { MapConcurrent } = require('../lib/generator/mapconcurrent');
 const { Pipeline } = require('../lib/generator/pipeline');
+const { promisify } = require('util');
+
+const sleep = promisify(setTimeout);
 
 async function toArray(items) {
     const result = [];
@@ -39,7 +43,7 @@ function checkAddBatchLessThan(batch, item) {
     return (length === 0) || (batch[length-1] < item);
 }
 
-describe.only('generator', function() {
+describe('generator', function() {
     describe('flatten', function() {
         it('empty', async function() {
             const flatten = new Flatten();
@@ -78,7 +82,7 @@ describe.only('generator', function() {
             assert.deepStrictEqual(result, [ 1, 2, 3, 4, 5, 6 ]);
         });
     });
-    describe('mapbatch', function() {
+    describe('batch', function() {
         it('empty', async function() {
             const batch = new Batch();
             const result = await toArray(batch.execute([]));
@@ -117,13 +121,97 @@ describe.only('generator', function() {
         });
     });
     describe('mapconcurrent', function() {
-        it('', async function() {
-          
+        it('empty', async function() {
+            const map = new MapConcurrent(value => value + 1);
+            const result = await toArray(map.execute([ ]));
+            assert.deepStrictEqual(result, [ ]);        
+        });
+        it('single-threaded', async function() {
+            const map = new MapConcurrent(value => value + 1);
+            const result = await toArray(map.execute([ 1, 2, 3, 4, 5 ]));
+            assert.deepStrictEqual(result, [ 2, 3, 4, 5, 6]);        
+        });
+        it('multi-unordered', async function() {
+            // unordered should return the first one completed, use sleep to force
+            // a reverse ordering
+            const map = new MapConcurrent(async value => {
+                await sleep(500 - value * 100);
+                return value;
+            }, { maxConcurrent: 3 });
+            const result = await toArray(map.execute([ 1, 2, 3 ]));
+            assert.deepStrictEqual(result, [ 3, 2, 1 ]);        
+        });
+        it('multi-ordered', async function() {
+            // ordered should keep the same order as the entries entered, use sleep
+            // to make sure this order is retained
+            const map = new MapConcurrent(async value => {
+                await sleep(500 - value * 100);
+                return value;
+            }, { ordered: true, maxConcurrent: 3 });
+            const result = await toArray(map.execute([ 1, 2, 3 ]));
+            assert.deepStrictEqual(result, [ 1, 2, 3 ]);        
+        });
+        it('multi-ordered-generator', async function() {
+            // ordered should keep the same order as the entries entered, use sleep
+            // to make sure this order is retained
+            const generator = toGenerator([1, 2, 3, 4, 5]);
+            const map = new MapConcurrent(async value => {
+                await sleep(500 - value * 100);
+                return value;
+            }, { maxConcurrent: 5 });
+            const result = await toArray(map.execute(generator));
+            assert.deepStrictEqual(result, [ 5, 4, 3, 2, 1 ]);        
+        });
+    });
+    describe('function', function() {
+        it('test', async function() {
+            const func = new AsyncGeneratorFunction();
+            const result = await toArray(func.execute([1,2,3]));
+            assert.deepStrictEqual(result, [ 1, 2, 3 ]);
         });
     });
     describe('pipeline', function() {
-        it('', async function() {
-          
+        it('empty-pipeline', async function() {
+            const pipeline = new Pipeline();
+            const result = await toArray(pipeline.execute([ 1, 2, 3, 4, 5 ]));
+            assert.deepStrictEqual(result, [ 1, 2, 3, 4, 5 ]);    
+        });
+        it('single-operation-pipeline', async function() {
+            const pipeline = new Pipeline({
+                execute: async function*(items) {
+                    for await (const item of items) {
+                        yield item + 1;
+                    }
+                }
+            });
+            const result = await toArray(pipeline.execute([ 1, 2, 3, 4, 5 ]));
+            assert.deepStrictEqual(result, [ 2, 3, 4, 5, 6 ]);    
+        });
+        it('two-operation-pipeline', async function() {
+            const pipeline = new Pipeline({
+                execute: async function*(items) {
+                    for await (const item of items) {
+                        yield item + 1;
+                    }
+                }
+            }, {
+                execute: async function*(items) {
+                    for await (const item of items) {
+                        yield item * 2;
+                    }
+                }
+            });
+            const result = await toArray(pipeline.execute([ 1, 2, 3, 4, 5 ]));
+            assert.deepStrictEqual(result, [ 4, 6, 8, 10, 12 ]);    
+        });
+        it('batch-map-flatten', async function() {
+            const pipeline = new Pipeline(
+                new Batch(() => true, { maxBatchLength: 3 }),
+                new MapConcurrent(value => value.reverse()),
+                new Flatten()
+            );
+            const result = await toArray(pipeline.execute([ 1, 2, 3, 4, 5 ]));
+            assert.deepStrictEqual(result, [ 3, 2, 1, 5, 4 ]);    
         });
     });
 });
