2f4d0e14aa05b99cdaf43faf9a5840b9a5905079
Bob van Manen <15641612+bobvanmanen@users.noreply.github.com>
2021-05-18 08:27:09 -0700

    Review fixes for Functional algorithms for async generators (#43)
    Co-authored-by: tmathern <60901087+tmathern@users.noreply.github.com>

--- test/generator.test.js
+++ test/generator.test.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Adobe. All rights reserved.
+ * Copyright 2021 Adobe. All rights reserved.
  * This file is licensed to you under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License. You may obtain a copy
  * of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -148,6 +148,24 @@ describe('generator', function() {
             });
         });
         describe('waitResult', function() {
+            it('empty-ordered', async function() {
+                try {
+                    const pending = [];
+                    await waitResult(pending, true);  
+                    assert.ok(false, 'waitResult expected to reject empty pending');
+                } catch (e) {
+                    assert.strictEqual(e.message, 'Invalid state: pending is empty');
+                }
+            });
+            it('empty-unordered', async function() {
+                try {
+                    const pending = [];
+                    await waitResult(pending, false);  
+                    assert.ok(false, 'waitResult expected to reject empty pending');
+                } catch (e) {
+                    assert.strictEqual(e.message, 'Invalid state: pending is empty');
+                }
+            });
             it('ordered', async function() {
                 const pending = [
                     sleepReturnValue(200, 1),
