80cb61b77b24a45f77d1b1cd769164dcdc51d771
Bob van Manen <bmanen@adobe.com>
2021-05-26 18:17:59 -0700

    Fixed assert.equal

--- test/aemmultipart.test.js
+++ test/aemmultipart.test.js
@@ -34,7 +34,7 @@ describe('multipart', function () {
             try {
                 await uploadAEMMultipartFile('test-transfer-file-1.dat');
             } catch (e) {
-                assert.equal(e.message, 'target not provided: undefined');
+                assert.strictEqual(e.message, 'target not provided: undefined');
             }
 
             try {
@@ -49,7 +49,7 @@ describe('multipart', function () {
             try {
                 await uploadAEMMultipartFile('test-transfer-file-2.dat', {});
             } catch (e) {
-                assert.equal(e.message, 'invalid number of target urls: undefined');
+                assert.strictEqual(e.message, 'invalid number of target urls: undefined');
             }
 
             try {
