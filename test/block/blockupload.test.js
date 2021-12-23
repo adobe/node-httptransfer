547c52567b0c8369aee3313634a258f0a8f9c8ab
Tania Mathern <mathern@adobe.com>
2021-12-23 09:54:51 -0800

    Add block upload tests

--- test/block/blockupload.test.js
+++ test/block/blockupload.test.js
@@ -23,13 +23,13 @@ const { BlockUpload } = require('../../lib/block/blockupload');
 const debug = require('debug');
 debug.enable('httptransfer*');
 
-describe('Block Upload', function() {
+describe('Block Upload', function () {
     afterEach(async function () {
-        assert.ok(nock.isDone(), 'check if all nocks have been used');
+        assert.ok(nock.isDone(), nock.pendingMocks(), 'check if all nocks have been used');
         nock.cleanAll();
     });
 
-    it('Block upload smoke test', async function() {
+    it('Block upload smoke test', async function () {
         console.log("block upload test");
 
         const HOST = 'http://test-aem-upload-201';
@@ -48,7 +48,7 @@ describe('Block Upload', function() {
         const blockUpload = new BlockUpload();
         const events = {
             filestart: [],
-            fileprogress:[],
+            fileprogress: [],
             fileend: []
         };
         blockUpload.on('transferPart', (data) => {
@@ -60,7 +60,7 @@ describe('Block Upload', function() {
         blockUpload.on('aftertransfer', (data) => {
             events.fileend.push(data);
         });
-        const targetUrl =  'http://test-aem-upload-201/path/to/file-1.jpg';
+        const targetUrl = 'http://test-aem-upload-201/path/to/file-1.jpg';
 
         await blockUpload.uploadFiles({
             uploadFiles: [{
@@ -104,7 +104,282 @@ describe('Block Upload', function() {
         });
     });
 
-    it('Block upload smoke test (multiple urls)', async function() {
+    it('Block upload smoke test, one max concurrency', async function () {
+        console.log("block upload test");
+
+        const HOST = 'http://test-aem-upload-201';
+        const testFile = Path.join(__dirname, 'file-1.jpg');
+        await fs.writeFile(testFile, 'hello world 123', 'utf8');
+
+        nock(HOST, {
+            reqheaders: {
+                'content-length': 15,
+                'content-type': 'image/jpeg'
+            }
+        })
+            .put('/path/to/file-1.jpg', 'hello world 123')
+            .reply(201);
+
+        const blockUpload = new BlockUpload();
+        const events = {
+            filestart: [],
+            fileprogress: [],
+            fileend: []
+        };
+        blockUpload.on('transferPart', (data) => {
+            events.filestart.push(data);
+        });
+        blockUpload.on('fileprogress', (data) => {
+            events.fileprogress.push(data);
+        });
+        blockUpload.on('aftertransfer', (data) => {
+            events.fileend.push(data);
+        });
+        const targetUrl = 'http://test-aem-upload-201/path/to/file-1.jpg';
+
+        await blockUpload.uploadFiles({
+            uploadFiles: [{
+                fileUrl: targetUrl,
+                filePath: testFile,
+                fileSize: 15
+            }],
+            headers: {
+                // Asset Compute passes through content-type header
+                'content-type': 'image/jpeg',
+            },
+            concurrent: true,
+            maxConcurrent: 1
+        });
+
+        try {
+            await fs.unlink(testFile);
+        } catch (e) { // ignore cleanup failures
+            console.log(e);
+        }
+
+        const fileEventData = {
+            fileName: 'file-1.jpg',
+            fileSize: 15,
+            targetFolder: '/path/to',
+            targetFile: '/path/to/file-1.jpg',
+            mimeType: "image/jpeg",
+            sourceFolder: __dirname,
+            sourceFile: testFile,
+        };
+
+        assert.deepStrictEqual(events.filestart[0], fileEventData);
+        assert.deepStrictEqual(events.fileprogress[0], {
+            ...fileEventData,
+            mimeType: "image/jpeg",
+            transferred: 15
+        });
+        assert.deepStrictEqual(events.fileend[0], {
+            ...fileEventData,
+            mimeType: "image/jpeg",
+        });
+    });
+
+    it('Block upload smoke test (multiple uploads)', async function () {
+        for (let i = 0; i < 3; i++) {
+
+            console.log("block upload test");
+
+            const HOST = 'http://test-aem-upload-201';
+            const testFile = Path.join(__dirname, 'file-1.jpg');
+            await fs.writeFile(testFile, 'hello world 123', 'utf8');
+
+            nock(HOST, {
+                reqheaders: {
+                    'content-length': 15,
+                    'content-type': 'image/jpeg'
+                }
+            })
+                .put('/path/to/file-1.jpg', 'hello world 123')
+                .reply(201);
+
+            const blockUpload = new BlockUpload();
+            const events = {
+                filestart: [],
+                fileprogress: [],
+                fileend: []
+            };
+            blockUpload.on('transferPart', (data) => {
+                events.filestart.push(data);
+            });
+            blockUpload.on('fileprogress', (data) => {
+                events.fileprogress.push(data);
+            });
+            blockUpload.on('aftertransfer', (data) => {
+                events.fileend.push(data);
+            });
+            const targetUrl = 'http://test-aem-upload-201/path/to/file-1.jpg';
+
+            await blockUpload.uploadFiles({
+                uploadFiles: [{
+                    fileUrl: targetUrl,
+                    filePath: testFile,
+                    fileSize: 15
+                }],
+                headers: {
+                    // Asset Compute passes through content-type header
+                    'content-type': 'image/jpeg',
+                },
+                concurrent: true,
+                maxConcurrent: 5
+            });
+
+            try {
+                await fs.unlink(testFile);
+            } catch (e) { // ignore cleanup failures
+                console.log(e);
+            }
+
+            const fileEventData = {
+                fileName: 'file-1.jpg',
+                fileSize: 15,
+                targetFolder: '/path/to',
+                targetFile: '/path/to/file-1.jpg',
+                mimeType: "image/jpeg",
+                sourceFolder: __dirname,
+                sourceFile: testFile,
+            };
+
+            assert.deepStrictEqual(events.filestart[0], fileEventData);
+            assert.deepStrictEqual(events.fileprogress[0], {
+                ...fileEventData,
+                mimeType: "image/jpeg",
+                transferred: 15
+            });
+            assert.deepStrictEqual(events.fileend[0], {
+                ...fileEventData,
+                mimeType: "image/jpeg",
+            });
+        }
+    });
+
+    it('Block upload smoke test (multiple upload promises)', async function () {
+        const uploadTasks = [];
+
+        for (let i = 0; i < 3; i++) {
+
+            console.log("block upload test");
+
+            const HOST = 'http://test-aem-upload-201';
+            const testFile = Path.join(__dirname, `file-1-${i}.jpg`);
+            await fs.writeFile(testFile, `hello world 123 ${i}`, 'utf8');
+
+            nock(HOST, {
+                reqheaders: {
+                    'content-length': 17,
+                    'content-type': 'image/jpeg'
+                }
+            })
+                .put(`/path/to/file-1-${i}.jpg`, `hello world 123 ${i}`)
+                .reply(201);
+
+            const blockUpload = new BlockUpload();
+            const events = {
+                filestart: [],
+                fileprogress: [],
+                fileend: []
+            };
+            blockUpload.on('transferPart', (data) => {
+                events.filestart.push(data);
+            });
+            blockUpload.on('fileprogress', (data) => {
+                events.fileprogress.push(data);
+            });
+            blockUpload.on('aftertransfer', (data) => {
+                events.fileend.push(data);
+            });
+            const targetUrl = `http://test-aem-upload-201/path/to/file-1-${i}.jpg`;
+
+            const uploadTask = blockUpload.uploadFiles({
+                uploadFiles: [{
+                    fileUrl: targetUrl,
+                    filePath: testFile,
+                    fileSize: 17
+                }],
+                headers: {
+                    // Asset Compute passes through content-type header
+                    'content-type': 'image/jpeg',
+                },
+                concurrent: true,
+                maxConcurrent: 2
+            });
+            uploadTasks.push[uploadTask];
+        }
+
+        await Promise.all(uploadTasks);
+
+        for(let i = 0; i < 3; i++){
+            const testFile = Path.join(__dirname, `file-1-${i}.jpg`);
+            await fs.unlink(testFile); // if unlink fails, there is an issue with the file
+        }
+    });
+
+    it('Block upload smoke test (multiple upload promises, 1 max concurrency)', async function () {
+        const uploadTasks = [];
+
+        for (let i = 0; i < 3; i++) {
+
+            console.log("block upload test");
+
+            const HOST = 'http://test-aem-upload-201';
+            const testFile = Path.join(__dirname, `file-1-${i}.jpg`);
+            await fs.writeFile(testFile, `hello world 123 ${i}`, 'utf8');
+
+            nock(HOST, {
+                reqheaders: {
+                    'content-length': 17,
+                    'content-type': 'image/jpeg'
+                }
+            })
+                .put(`/path/to/file-1-${i}.jpg`, `hello world 123 ${i}`)
+                .reply(201);
+
+            const blockUpload = new BlockUpload();
+            const events = {
+                filestart: [],
+                fileprogress: [],
+                fileend: []
+            };
+            blockUpload.on('transferPart', (data) => {
+                events.filestart.push(data);
+            });
+            blockUpload.on('fileprogress', (data) => {
+                events.fileprogress.push(data);
+            });
+            blockUpload.on('aftertransfer', (data) => {
+                events.fileend.push(data);
+            });
+            const targetUrl = `http://test-aem-upload-201/path/to/file-1-${i}.jpg`;
+
+            const uploadTask = blockUpload.uploadFiles({
+                uploadFiles: [{
+                    fileUrl: targetUrl,
+                    filePath: testFile,
+                    fileSize: 17
+                }],
+                headers: {
+                    // Asset Compute passes through content-type header
+                    'content-type': 'image/jpeg',
+                },
+                concurrent: true,
+                maxConcurrent: 1
+            });
+            uploadTasks.push[uploadTask];
+        }
+
+        await Promise.all(uploadTasks);
+
+        for(let i = 0; i < 3; i++){
+            const testFile = Path.join(__dirname, `file-1-${i}.jpg`);
+            await fs.unlink(testFile); // if unlink fails, there is an issue with the file
+        }
+    });
+
+    it('Block upload smoke test (multiple urls)', async function () {
         console.log("block upload test");
 
         const HOST = 'http://test-aem-upload-201';
@@ -131,7 +406,7 @@ describe('Block Upload', function() {
         const blockUpload = new BlockUpload();
         const events = {
             filestart: [],
-            fileprogress:[],
+            fileprogress: [],
             fileend: []
         };
         blockUpload.on('transferPart', (data) => {
@@ -143,7 +418,7 @@ describe('Block Upload', function() {
         blockUpload.on('aftertransfer', (data) => {
             events.fileend.push(data);
         });
-        const targetUrls =  [
+        const targetUrls = [
             'http://test-aem-upload-201/path/to/file-1-1.jpg',
             'http://test-aem-upload-201/path/to/file-1-2.jpg',
             'http://test-aem-upload-201/path/to/file-1-3.jpg',
@@ -200,7 +475,7 @@ describe('Block Upload', function() {
         });
     });
 
-    it('Block upload no filePath or blob error', function() {
+    it('Block upload no filePath or blob error', function () {
         const blobkUpload = new BlockUpload();
         assert.rejects(() => blobkUpload.uploadFiles({
             uploadFiles: [{
@@ -211,5 +486,4 @@ describe('Block Upload', function() {
             name: 'IllegalArgumentException'
         });
     });
-
 });
