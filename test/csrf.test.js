dc765818d26a83429c8aabf19e995b67db4b7956
Yu-An Lin <yualin@adobe.com>
2022-01-12 13:36:55 -0800

    add unit test

---
+++ test/csrf.test.js
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2022 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+/* eslint-env mocha */
+
+'use strict';
+
+const nock = require('nock');
+const assert = require('assert');
+const { getCSRFToken } = require('../lib/csrf');
+
+describe('csrf', function () {
+    beforeEach(async function () {
+        nock.cleanAll();
+    });
+
+    afterEach(async function () {
+        assert.ok(nock.isDone(), 'check if all nocks have been used');
+        nock.cleanAll();
+    });
+
+    it('success', async function () {
+        nock('http://test')
+            .get('/libs/granite/csrf/token.json')
+            .reply(200, '{ "token": "test-csrf-token" }');
+
+        const token = await getCSRFToken("http://test");
+        assert.strictEqual(token, 'test-csrf-token');
+    });
+
+    it('throw expected error', async function () {
+        nock('http://test')
+            .get('/libs/granite/csrf/token.json')
+            .reply(400, '');
+
+        assert.rejects(getCSRFToken("http://test"),
+            `Error: Fail to get CSRF token with err Error: GET 'http://test/libs/granite/csrf/token.json' failed with status 400`);
+    });
+});
